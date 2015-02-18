eeac2761ab9719fb034ae606bbf5495e3559c880
Константин <kostya_po@mail.ru>
2015-02-18 23:57:37 +0700

    add plugin

---
+++ Assets/Plugins/ShowAppstore/ShowAppstoreBunch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ShowAppstoreBunch : MonoBehaviour {
+
+    
+    private static string BUNCH = "ShowAppstoreBunch";
+    
+	private static ShowAppstoreBunch INSTANCE = null;
+    
+	public static ShowAppstoreBunch GetInstance() {
+        return INSTANCE;
+    }
+    
+    void Awake() {
+		if (ShowAppstoreBunch.INSTANCE == null) {
+			ShowAppstoreBunch.INSTANCE = this;
+            BunchManager.registerBunch(BUNCH);
+            GameObject.DontDestroyOnLoad(this);
+        } else {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+    
+    public void ShowAppstore(string text) {
+        NativeGateway.dispatch(
+            BUNCH,
+			"showAppstore",
+            new Dictionary<string, object> () {{"text", text}}
+        );
+    }
+}
